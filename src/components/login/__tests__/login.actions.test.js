defdae677bd0d3611e596b39b05da2ad133e7353
Steven <sxu@inflightcorp.com>
2019-07-24 17:25:18 -0400

    Setup tests

---
+++ src/components/login/__tests__/login.actions.test.js
@@ -0,0 +1,16 @@
+import loginUser from '../login.actions'
+import { LOGIN_USER } from '../login.types'
+
+describe('Actions', () => {
+  it('should create an action to login a user', () => {
+    const user = {
+      email: "KevinZhang@gmail.com",
+      password: "Keith"
+    };
+    const expectedAction = {
+      type: LOGIN_USER,
+      user
+    };
+    expect(loginUser(user)).toEqual(expectedAction);
+  });
+});
